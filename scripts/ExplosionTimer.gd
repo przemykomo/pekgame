f019af5642a1baef1727203e754d8abe175cb0e8
xpiotreee <piotrowski.00@wp.pl>
2022-04-27 13:39:13 +0200

    Synchronize rigidbodies

--- scripts/ExplosionTimer.gd
+++ scripts/ExplosionTimer.gd
@@ -1,8 +1,13 @@
 extends Timer
 
 func _on_ExplosionTimer_timeout():
+	get_node("../CollisionShape").queue_free()
+	get_node("../MeshInstance").queue_free()
 	get_node("../Particles").emitting = true
 	for physicsBody in get_node("../ExplosionArea").get_overlapping_bodies():
 		if physicsBody != get_parent() && physicsBody is RigidBody:
 			var distance = physicsBody.global_transform.origin + Vector3(0, 0.5, 0) - get_parent().global_transform.origin
 			physicsBody.apply_central_impulse(distance.normalized() * (1 - distance.length() / 7) * 50)
+	
+	yield(get_tree().create_timer(1), "timeout")
+	get_parent().queue_free()
